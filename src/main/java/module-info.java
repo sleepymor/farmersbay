6d9e0867482f774115da2346594395950c159454
sleepy <nemorzgm.05@gmail.com>
2025-06-10 20:19:56 +0800

    Update module-info and refactor ChooseUser.fxml controller path for improved structure

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,30 @@ module name {
     requires javafx.fxml;
     requires java.sql;
     requires org.xerial.sqlitejdbc;
-    
-    opens projects.farmersbay.testfxml to javafx.fxml;
+
     exports projects.farmersbay.testfxml;
     exports projects.farmersbay;
+    exports projects.farmersbay.view.Auth.Admin;
+    exports projects.farmersbay.view.Auth;
+    exports projects.farmersbay.controller.Admin;
+    exports projects.farmersbay.model;
+    exports projects.farmersbay.view.Admin;
+    exports projects.farmersbay.view.Auth.Public;
+    exports projects.farmersbay.controller.Public;
+    exports projects.farmersbay.controller;
+    // exports projects.farmersbay.view;
+
+    
+    opens projects.farmersbay.testfxml to javafx.fxml;
     opens projects.farmersbay to javafx.fxml;
+    opens projects.farmersbay.view.Auth.Admin to javafx.fxml;
+    opens projects.farmersbay.view.Auth to javafx.fxml;
+    opens projects.farmersbay.controller.Admin to javafx.fxml;
+    opens projects.farmersbay.model to javafx.fxml;
+    opens projects.farmersbay.view.Admin to javafx.fxml;
+    opens projects.farmersbay.view.Auth.Public to javafx.fxml;
+    opens projects.farmersbay.controller.Public to javafx.fxml;
+    opens projects.farmersbay.controller to javafx.fxml;
+    // opens projects.farmersbay.view to javafx.fxml;
+   
 }
